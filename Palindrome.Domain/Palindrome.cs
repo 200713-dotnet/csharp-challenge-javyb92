b6205b351945c2c076f0286f88f051a1855cfdc9
javyb92 <javier.a.barragan92@gmail.com>
2020-07-20 16:03:11 -0500

    wrote palindrom file

--- Palindrome.Domain/Palindrome.cs
+++ Palindrome.Domain/Palindrome.cs
@@ -6,7 +6,38 @@ namespace Palindrome.Domain
     {
         static void Main(string[] args)
         {
+            string initial = "";
+            Console.WriteLine("Enter a string");
+            string userinput = Console.ReadLine();
+            int userinputLength = userinput.Length;
+            if (userinputLength == 0)
+            {
+                Console.WriteLine("Please enter a string");
 
+            }
+
+            else
+
+            {
+
+                for (int j = userinputLength - 1; j >= 0; j--)
+                {
+                    initial = initial + userinput[j];
+                }
+
+                if (initial == userinput)
+                {
+                    Console.WriteLine(userinput + " is palindrome");
+                }
+                else
+                {
+                    Console.WriteLine(userinput
+                        + " is not a palindrome");
+                }
+
+
+                Console.Read();
+            }
         }
     }
 
